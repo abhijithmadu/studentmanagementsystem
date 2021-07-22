1b7658a781ed352a8475cb60f1c763987e6b53db
abhijithmadu <abhimnair3@gmail.com>
2021-07-22 17:26:18 +0530

    first commit

---
+++ student_management_app/models.py
@@ -0,0 +1,145 @@
+from django.contrib.auth.models import AbstractUser
+from os import name
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class AdminHOD(models.Model):
+    id=models.AutoField(primary_key=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class Staffs(models.Model):
+    id=models.AutoField(primary_key=True)
+    address=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    fcm_token=models.TextField(default="")
+    objects=models.Manager()
+    
+class Courses(models.Model):
+    id=models.AutoField(primary_key=True)
+    course_name=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class Subjects(models.Model):
+    id=models.AutoField(primary_key=True)
+    subject_name=models.CharField(max_length=255)
+    course_id=models.ForeignKey(Courses,on_delete=models.CASCADE,default=1)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class Students(models.Model):
+    id=models.AutoField(primary_key=True)
+    gender=models.CharField(max_length=255)
+    profile_pic=models.FileField()
+    address=models.TextField()
+    course_id=models.ForeignKey(Courses,on_delete=models.DO_NOTHING)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    fcm_token=models.TextField(default="")
+    objects = models.Manager()
+
+class Attendance(models.Model):
+    id=models.AutoField(primary_key=True)
+    subject_id=models.ForeignKey(Subjects,on_delete=models.DO_NOTHING)
+    attendance_date=models.DateField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class AttendanceReport(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.DO_NOTHING)
+    attendance_id=models.ForeignKey(Attendance,on_delete=models.CASCADE)
+    status=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class LeaveReportStudent(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.CASCADE)
+    leave_date=models.CharField(max_length=255)
+    leave_message=models.TextField()
+    leave_status=models.IntegerField(default=0)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class LeaveReportStaff(models.Model):
+    id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
+    leave_date = models.CharField(max_length=255)
+    leave_message = models.TextField()
+    leave_status = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class FeedBackStudent(models.Model):
+    id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feedback_reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class FeedBackStaffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feedback_reply=models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class NotificationStudent(models.Model):
+    id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class NotificationStaffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class StudentResult(models.Model):
+    id=models.AutoField(primary_key=True)
+    student_id=models.ForeignKey(Students,on_delete=models.CASCADE)
+    subject_id=models.ForeignKey(Subjects,on_delete=models.CASCADE)
+    subject_exam_marks=models.FloatField(default=0)
+    subject_assignment_marks=models.FloatField(default=0)
+    created_at=models.DateField(auto_now_add=True)
+    updated_at=models.DateField(auto_now_add=True)
+    objects=models.Manager()
+
+class OnlineClassRoom(models.Model):
+    id=models.AutoField(primary_key=True)
+    room_name=models.CharField(max_length=255)
+    room_pwd=models.CharField(max_length=255)
+    subject=models.ForeignKey(Subjects,on_delete=models.CASCADE)
+    started_by=models.ForeignKey(Staffs,on_delete=models.CASCADE)
+    is_active=models.BooleanField(default=True)
+    created_on=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+
+
+

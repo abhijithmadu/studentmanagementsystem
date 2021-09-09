6809421e55b6a6af9ca037ad5405a8e71f4304da
abhijithmadu <abhimnair3@gmail.com>
2021-09-09 18:34:53 +0530

    static removed3

--- student_management_app/static/messages.js
+++ student_management_app/static/messages.js
@@ -48,7 +48,7 @@ let propic="{{login_pro.logo.url}}"
 // }
 // let endpoint = 'ws://' + loc.host +":8001"+ loc.pathname
 
-var socket = new WebSocket('ws://smsportal.realabhijith.tech:8001/chat/')
+var socket = new WebSocket('ws://realabhijith.tech:8001/chat/')
 // var socket = new WebSocket(endpoint)
 
 socket.onopen = async function(e){

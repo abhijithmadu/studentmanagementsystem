9006baf3f6951d166f0fd51ae03fcd4fa81b73af
abhijithmadu <abhimnair3@gmail.com>
2021-09-09 17:40:11 +0530

    ws4

--- student_management_app/static/messages.js
+++ student_management_app/static/messages.js
@@ -46,9 +46,9 @@ let loc = window.location
 // else{
 //     wsStart = 'ws://'
 // }
-let endpoint = 'wss://' + loc.host +":8001"+ loc.pathname
+// let endpoint = 'ws://' + loc.host +":8001"+ loc.pathname
 
-// var socket = new WebSocket('ws://realabhijith.tech:8001/chat/')
+var socket = new WebSocket('wss://realabhijith.tech:8001/chat/')
 var socket = new WebSocket(endpoint)
 
 socket.onopen = async function(e){
